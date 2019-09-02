ba1d395e3cef475a9c5f580c85f8f21f2328ae97
liuxz <1510822551@qq.com>
2019-09-02 11:46:25 +0800

    ok

--- src/main/java/com/dapeng/flow/controller/BackController.java
+++ src/main/java/com/dapeng/flow/controller/BackController.java
@@ -35,17 +35,16 @@ public class BackController {
     @Autowired
     private TaskHandler taskHandler;
 
-
     @ResponseBody
     @RequestMapping(value = "/withdraw", method = RequestMethod.POST)
-    @ApiOperation(value = "任务撤回",notes = "注意：当前与目标定义ID为设计模板时任务对应的ID,而非数据主键ID",produces = "application/json")
+    @ApiOperation(value = "任务撤回",notes = "注意：当前与目标定义Key为设计模板时任务对应的ID,而非数据主键ID",produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "processInstanceId", value = "流程实例ID", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "currentActivityId", value = "当前任务定义ID", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "newActivityId", value = "目标任务定义ID", required = true, dataType = "String")
+            @ApiImplicitParam(name = "currentTaskKey", value = "当前任务定义Key", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "targetTaskKey", value = "目标任务定义Key", required = true, dataType = "String")
     })
-    public ResponseData withdraw(String processInstanceId, String currentActivityId, String newActivityId) {
-        taskHandler.withdraw(processInstanceId, currentActivityId, newActivityId);
+    public ResponseData withdraw(String processInstanceId, String currentTaskKey, String targetTaskKey) {
+        taskHandler.withdraw(processInstanceId, currentTaskKey, targetTaskKey);
         return ResponseData.success("任务撤回成功");
     }
 
